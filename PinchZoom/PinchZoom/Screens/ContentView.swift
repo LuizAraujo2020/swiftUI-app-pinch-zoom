873e0288c62871aec75d5aa0058df3b9a579a92f
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-26 20:28:11 -0300

    SwiftUI Drag Gesture

--- PinchZoom/PinchZoom/Screens/ContentView.swift
+++ PinchZoom/PinchZoom/Screens/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var isAnimating = false
     @State private var imageScale = CGFloat(1)
+    @State private var imageOffset = CGSize.zero
 
     var body: some View {
         NavigationStack {
@@ -24,27 +25,46 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
+                // MARK: - TAP GESTURE
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                // MARK: - DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: zstack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
             .onAppear {
-//                withAnimation(.linear(duration: 1)) {
-                    isAnimating = true
-//                }
+                isAnimating = true
             }
         } //: nav
+    }
+
+    private func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
     }
 }
 
