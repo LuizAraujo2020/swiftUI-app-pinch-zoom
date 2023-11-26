743486765b584dbd87dd27a0ec9c9913f2df175e
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-25 23:41:04 -0300

    Setting up the new iOS iPhone and iPad project

---
+++ PinchZoom/PinchZoom/PinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomApp.swift
+//  PinchZoom
+//
+//  Created by Luiz Araujo on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
