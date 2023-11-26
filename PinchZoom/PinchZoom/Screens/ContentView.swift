a2fb184e4f3f0112acb300d3fd9e92bdc60af5aa
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-11-26 20:16:19 -0300

    SwiftUI Double Tap Gesture

--- PinchZoom/PinchZoom/Screens/ContentView.swift
+++ PinchZoom/PinchZoom/Screens/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAnimating = false
+    @State private var imageScale = CGFloat(1)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .animation(.none, value: isAnimating)
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            } //: zstack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+//                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+//                }
+            }
+        } //: nav
     }
 }
 
